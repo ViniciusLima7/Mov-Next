226adf1b3c651b404bd29952e16e17094e794214
Vinicius Lima <viniciuslima.thoughtworks@latam.com>
2025-10-27 22:20:50 -0300

    `Added alt attribute to img tag in ChallengeBox component`

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -27,7 +27,7 @@ export function ChallengeBox() {
           <header>Ganhe {activeChallenge.amount} xp</header>
 
           <main>
-            <img src={`icons/${activeChallenge.type}.svg`}></img>
+            <img src={`icons/${activeChallenge.type}.svg`} alt="Desafio" />
             <strong>Novo Desafio</strong>
             <p>{activeChallenge.description}</p>
           </main>
