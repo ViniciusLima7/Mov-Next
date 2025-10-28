38021c6d24d70fb06081b4b52a8b135b62f59e53
Vinicius Lima <viniciuslima.thoughtworks@latam.com>
2025-10-27 22:55:01 -0300

    fix: add suppressHydrationWarning to prevent hydration mismatch errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -21,7 +21,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
